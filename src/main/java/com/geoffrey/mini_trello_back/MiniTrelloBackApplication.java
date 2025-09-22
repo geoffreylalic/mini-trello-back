91b4544e724857e0f675bfef3fe4a14bcaf7385b
Geoffrey <geoffrey0151@gmail.com>
2025-09-22 16:15:13 +0200

    spring initializer first commit

---
+++ src/main/java/com/geoffrey/mini_trello_back/MiniTrelloBackApplication.java
@@ -0,0 +1,13 @@
+package com.geoffrey.mini_trello_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniTrelloBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniTrelloBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geoffrey/mini_trello_back/MiniTrelloBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geoffrey.mini_trello_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniTrelloBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
