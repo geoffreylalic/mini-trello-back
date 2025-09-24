c3d1ababdf46456d06c6856c9f5915975afa0d11
Geoffrey <geoffrey0151@gmail.com>
2025-09-24 17:51:07 +0200

    added user mapper

---
+++ src/main/java/com/geoffrey/mini_trello_back/user/UserMapper.java
@@ -0,0 +1,21 @@
+package com.geoffrey.mini_trello_back.user;
+
+import com.geoffrey.mini_trello_back.user.dto.CreateUserDto;
+import com.geoffrey.mini_trello_back.user.dto.UserResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public User toUser(CreateUserDto userDto) {
+        User user = new User();
+        user.setFirstName(userDto.firstName());
+        user.setLastName(userDto.lastName());
+        user.setPassword(userDto.password());
+        user.setEmail(userDto.email());
+        return user;
+    }
+
+    public UserResponseDto toUserResponse(User user) {
+        return new UserResponseDto(user.getId(), user.getEmail(), user.getFirstName(), user.getLastName());
+    }
+}
