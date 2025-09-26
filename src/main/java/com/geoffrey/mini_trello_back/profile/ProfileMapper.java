c5b4f77fa268df6b6668822921a1e8e758c9c4dd
Geoffrey <geoffrey0151@gmail.com>
2025-09-26 16:55:42 +0200

    added profile mapper

---
+++ src/main/java/com/geoffrey/mini_trello_back/profile/ProfileMapper.java
@@ -0,0 +1,55 @@
+package com.geoffrey.mini_trello_back.profile;
+
+import com.geoffrey.mini_trello_back.profile.dto.ProfileResponseDto;
+import com.geoffrey.mini_trello_back.project.Project;
+import com.geoffrey.mini_trello_back.task.Task;
+import com.geoffrey.mini_trello_back.user.User;
+import com.geoffrey.mini_trello_back.user.UserMapper;
+import com.geoffrey.mini_trello_back.user.UserRepository;
+import com.geoffrey.mini_trello_back.user.dto.UserResponseDto;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Component
+public class ProfileMapper {
+
+    UserMapper userMapper;
+    UserRepository userRepository;
+
+
+    public ProfileMapper(UserMapper userMapper, UserRepository userRepository) {
+        this.userMapper = userMapper;
+        this.userRepository = userRepository;
+    }
+
+    public Profile userToProfile(User user, @Past @NotNull LocalDate dateOfBirth, @NotNull Role role) {
+        Profile profile = new Profile();
+        profile.setUser(user);
+        profile.setDateOfBirth(dateOfBirth);
+        profile.setRole(role);
+        return profile;
+    }
+
+    public ProfileResponseDto toProfileResponseDto(Profile profile) {
+
+        Integer id = profile.getId();
+        UserResponseDto userResponseDto = userMapper.toUserResponse(profile.getUser());
+        LocalDate dateOfBirth = profile.getDateOfBirth();
+        String role = profile.getRole().name();
+        List<Project> projects = profile.getProjects();
+        List<Task> tasks = profile.getTasks();
+
+        return new ProfileResponseDto(
+                id,
+                userResponseDto,
+                dateOfBirth,
+                role,
+                projects,
+                tasks
+        );
+    }
+}
