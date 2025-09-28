784a6b609c2c607aa8692bf4f1ac8cade7aa5439
Geoffrey <geoffrey0151@gmail.com>
2025-09-28 15:31:04 +0200

    fixed error message

--- src/main/java/com/geoffrey/mini_trello_back/profile/exceptions/ProfileUserAlreadyExistsException.java
+++ src/main/java/com/geoffrey/mini_trello_back/profile/exceptions/ProfileUserAlreadyExistsException.java
@@ -2,6 +2,6 @@ package com.geoffrey.mini_trello_back.profile.exceptions;
 
 public class ProfileUserAlreadyExistsException extends RuntimeException {
     public ProfileUserAlreadyExistsException(Integer userId) {
-        super("The user with the id: " + String.valueOf(userId) + ", already has a profile");
+        super("The profile with the id:" + String.valueOf(userId) + " , already has a profile");
     }
 }
