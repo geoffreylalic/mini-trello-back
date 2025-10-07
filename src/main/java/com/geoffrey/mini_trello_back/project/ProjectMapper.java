9a88ffa2d5ccabb7c8ee7c6c1ffacc52cae07a0a
Geoffrey <geoffrey0151@gmail.com>
2025-10-07 10:51:34 +0200

    removed unused methods on ProjectMapper

--- src/main/java/com/geoffrey/mini_trello_back/project/ProjectMapper.java
+++ src/main/java/com/geoffrey/mini_trello_back/project/ProjectMapper.java
@@ -2,20 +2,17 @@ package com.geoffrey.mini_trello_back.project;
 
 import com.geoffrey.mini_trello_back.profile.Profile;
 import com.geoffrey.mini_trello_back.profile.ProfileMapper;
-import com.geoffrey.mini_trello_back.profile.ProfileRepository;
 import com.geoffrey.mini_trello_back.profile.dto.SimpleProfileResponseDto;
 import com.geoffrey.mini_trello_back.project.dto.CreateProjectDto;
 import com.geoffrey.mini_trello_back.project.dto.ProjectResponseDto;
 import com.geoffrey.mini_trello_back.project.dto.SimpleProjectDto;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Component
 public class ProjectMapper {
     private final ProfileMapper profileMapper;
 
-    public ProjectMapper(ProfileMapper profileMapper, ProfileRepository profileRepository) {
+    public ProjectMapper(ProfileMapper profileMapper) {
         this.profileMapper = profileMapper;
     }
 
@@ -27,20 +24,13 @@ public class ProjectMapper {
         return new ProjectResponseDto(id, name, description, owner);
     }
 
-    public List<ProjectResponseDto> toListProjectResponseDto(List<Project> projects) {
-        return projects.stream().map(this::toProjectResponseDto).toList();
-    }
 
     public SimpleProjectDto toSimpleProjectDto(Project project) {
         return new SimpleProjectDto(project.getId(), project.getName(), project.getDescription());
     }
 
-    public List<SimpleProjectDto> toListSimpleProjectDto(List<Project> projects) {
-        return projects.stream().map(this::toSimpleProjectDto).toList();
-    }
 
     public Project toProject(CreateProjectDto projectDto, Profile owner) {
-        Integer profileId = projectDto.profileId();
         String name = projectDto.name();
         String description = projectDto.description();
 
