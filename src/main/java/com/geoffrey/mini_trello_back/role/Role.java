373ebac1b341686a9f7c8ee452283d17bc3cd5ce
Geoffrey <geoffrey0151@gmail.com>
2025-10-08 17:44:02 +0200

    added new table roles

---
+++ src/main/java/com/geoffrey/mini_trello_back/role/Role.java
@@ -0,0 +1,34 @@
+package com.geoffrey.mini_trello_back.role;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
