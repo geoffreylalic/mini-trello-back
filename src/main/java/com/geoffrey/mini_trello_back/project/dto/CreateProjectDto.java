a1cd06abf9d2b4bf2b23e328f44c29faa6fd1123
Geoffrey <geoffrey0151@gmail.com>
2025-10-21 14:21:52 +0200

    removed unused parameter on project creation

--- src/main/java/com/geoffrey/mini_trello_back/project/dto/CreateProjectDto.java
+++ src/main/java/com/geoffrey/mini_trello_back/project/dto/CreateProjectDto.java
@@ -3,6 +3,5 @@ package com.geoffrey.mini_trello_back.project.dto;
 import jakarta.validation.constraints.NotNull;
 
 public record CreateProjectDto(@NotNull String name,
-                               String description,
-                               @NotNull Integer profileId) {
+                               String description) {
 }
